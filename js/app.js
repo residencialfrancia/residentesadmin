806e1645ee2038c619ba9bff99734af452bea7b2
residencialfrancia <residencialfranciasn@gmail.com>
2026-08-04 23:47:41 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  * APP.JS - Lógica de Dashboard Multitabs
  */
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbw3CFISeZHijXfIbiHbLHeROagg3R6s-lSzFtzJj6GAmEG6ptoHTrbtmy7majgMAZOA4w/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzpHZgl9ZUD_NCVXR1qN45ohwiObhWgmpxgUzK883ySzC30LPRLrD3iFxKB1F22fcC3Qw/exec';
 const FALLBACK_IMAGE = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
 
 let globalActivos = [];
